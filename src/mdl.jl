513842b2e6a0f3b4fb2195d44a67cb70902cd743
Art Wild <wildart@gmail.com>
2015-02-08 12:16:44 -0500

    updated LMC model

--- src/mdl.jl
+++ src/mdl.jl
@@ -1,22 +1,23 @@
-function MDLength(M::Manifold, X::Matrix; P::Float64 = 32.0, T::Symbol = :Gausian, bins::Int = 20)
-    n = size(X,1)
-    m = indim(M)
-    L = m == 0 ? P*n : P*(m*n - (m-1)*(m-2)/2.)
+function MDLength(M::Manifold, X::Matrix; P::Float64 = 32.0, T::Symbol = :Gaussian, bins::Int = 20)
+    n = size(X,1) # space dimension
+    m = indim(M)  # manifold dimension
+    l = outdim(M) # cluster size
+    L = m == 0 ? P*n : P*(n + m*(n - (m+1)/2. + l))
 
     gusent(n, Σd) = n*(1+log(2π))/2 + (Σd == 0.0 ? 0.0 : log(Σd))
 
     E = 0.0
     if T == :Uniform && m > 0
         E = -float(n)*log(separation(M).threshold)
-    elseif T == :Gausian && m > 0
+    elseif T == :Gaussian && m > 0
         B = projection(M)
         Xtr = X .- mean(M)
         OP = B*B'*Xtr # points projected to manifold subspace
         Σ = OP*OP'
-        E += gusent(m, det(Σ)) * outdim(M)
+        E += gusent(m, det(Σ)) * l
         OP = (eye(n) - B*B')*Xtr # points projected to orthoganal complement subspace
         Σ = OP*OP'
-        E += gusent(m, det(Σ)) * outdim(M)
+        E += gusent(m, det(Σ)) * l
     elseif T == :Empirical # for 0D manifold only empirical estimate is avalible
         Xtr = X .- mean(M)
         F = svdfact(Xtr)
@@ -31,7 +32,7 @@ function MDLength(M::Manifold, X::Matrix; P::Float64 = 32.0, T::Symbol = :Gausia
                 Cx = linspace(Cmin, Cmax, bins)
                 h = hist(C,Cx)[2]
                 h /= sum(h)
-                E += -sum(map(x-> x == 0. ? 0. : x*log2(x), h)) * outdim(M)
+                E += -sum(map(x-> x == 0. ? 0. : x*log2(x), h)) * l
             end
         end
     elseif T == :Center
@@ -41,9 +42,8 @@ function MDLength(M::Manifold, X::Matrix; P::Float64 = 32.0, T::Symbol = :Gausia
                 E += log2(abs(Xtr[p,i]) + 1)
             end
         end
-    else
-        E = outdim(M)*n
+    else # No encoding for 0D manfiold
+        E = P*n*(l-1)
     end
-    #warn("H:$(L) + D:$(E)")
     return L+E
 end
